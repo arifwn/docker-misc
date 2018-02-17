09be40a8670edb9a429a60a26e9782bbb7b2f0bf
Arif Widi Nugroho <arif@sainsmograf.com>
2018-02-18 01:42:49 +0700

    updated build scripts

--- build-images.sh
+++ build-images.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 docker build -t arifwn/php:5.6-apache images/php5.6
 
 docker build -t arifwn/php:7.2-apache images/php7.2
--- push-images.sh
+++ push-images.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 docker push arifwn/php
 
 docker push arifwn/wordpress
