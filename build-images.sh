ce35705a451b53f910c12bfbb82bb680c69e99ed
Arif Widi Nugroho <arif@sainsmograf.com>
2018-02-21 08:37:42 +0700

    added wordpress on php5.6

--- build-images.sh
+++ build-images.sh
@@ -7,3 +7,5 @@ docker build -t arifwn/php:5.6-apache images/php5.6
 docker build -t arifwn/php:7.2-apache images/php7.2
 
 docker build -t arifwn/wordpress:7.2-apache images/wordpress-php7.2
+
+docker build -t arifwn/wordpress:5.6-apache images/wordpress-php5.6
